ea9c33239e193a90ea731830aa99e0e34ad735d4
ywan2205 <39372315+ywan2205@users.noreply.github.com>
2018-05-17 21:27:56 +1000

    增加了根据A0输入判断是否有车的功能
    如果检测到车，LCD上面一行显示four spare room
    如果没检测到，显示hello world

--- LCD.ino
+++ LCD.ino
@@ -1,42 +1,8 @@
 /*
   LiquidCrystal Library - Hello World
 
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
  This sketch prints "Hello World!" to the LCD
  and shows the time.
-
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
-、
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystalHelloWorld
 
 */
 
@@ -53,12 +19,32 @@ void setup() {
   lcd.begin(16, 2);
   // Print a message to the LCD.
   lcd.print("hello, world!");
+  //detect input port A0
+  pinMode(A0,INPUT);
 }
 
 void loop() {
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
+  LCDhowmuchspare();
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
   lcd.print(millis() / 1000);
 }
+
+void LCDhowmuchspace(){
+  int val1 = 0;
+  val1 = analogRead(A0);
+  val1 = (val1>300)? 90 : 0 ;
+  if(val1 == 90){
+    lcd.setCursor(0, 0);
+    lcd.print("four spare room");
+  }
+  else{
+    lcd.setCursor(0, 0);
+    lcd.print("Hello world");
+  }
+    
+}
+  
+  
